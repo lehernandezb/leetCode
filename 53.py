b67c69e450373a3625c33876afb4c8dd6a1f9750
Luis <luis.hernandez1@ucalgary.ca>
2026-06-10 17:40:14 -0600

    Solution to question 53

---
+++ 53.py
@@ -0,0 +1,20 @@
+class Solution(object):
+
+    # O(n) solution to question 53. The main idea is to max a choice if we should continue the sub string or start a new one.
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
+        maxSum = nums[0]
+        currentSum = nums[0]
+
+        for index in range(1, len(nums)):
+            if currentSum + nums[index] > nums[index]:
+                currentSum += nums[index] 
+            else:
+                currentSum = nums[index]
+            if currentSum > maxSum: maxSum = currentSum 
+
+        return maxSum
