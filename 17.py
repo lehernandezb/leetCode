2e06d0dc5e46d88119153aaa9cae294a7ab4e26f
Luis <luis.hernandez1@ucalgary.ca>
2026-06-18 20:39:38 -0600

    Solution to question 18

--- 17.py
+++ 17.py
@@ -1,7 +1,34 @@
 class Solution(object):
+
+    # First time doing a recursive method to slove a question. I feel like this was the only way that made sense in my head lol
     def letterCombinations(self, digits):
         """
         :type digits: str
         :rtype: List[str]
         """
+
+        if not digits: return []
+
+        key = {
+            '2': 'abc',
+            '3': 'def',
+            '4': 'ghi',
+            '5': 'jkl',
+            '6': 'mno',
+            '7': 'pqrs',
+            '8': 'tuv',
+            '9': 'wxyz',
+        }
+        result = []
+
+        def recursiveCombinations(combo, nextDigit):
+            if not nextDigit:
+                result.append(combo)
+                return
+            
+            for letter in key[nextDigit[0]]:
+                recursiveCombinations(combo + letter, nextDigit[1:])
         
+        recursiveCombinations("", digits)
+
+        return result
---
+++ 18.py
@@ -0,0 +1,44 @@
+class Solution(object):
+
+    # Solution to question 18
+    def fourSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        
+        nums.sort()
+        output = []
+        n = len(nums)
+
+        for i in range(n - 3):
+
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
+            for j in range(i + 1, n - 2):
+                if j > i + 1 and nums[j] == nums[j - 1]:
+                    continue
+
+                left = j + 1
+                right = n - 1
+
+                while (left < right):
+                    total = nums[i] + nums[j] + nums[left] + nums[right]
+
+                    if total == target:
+                        output.append([nums[i], nums[j], nums[left], nums[right]])
+                        left += 1
+                        right -= 1
+
+                        while left < right and nums[left] == nums[left - 1]:
+                            left += 1
+                        while right > left and nums[right] == nums[right + 1]:
+                            right -= 1
+                    elif total < target:
+                        left += 1
+                    else:
+                        right -= 1
+
+        return output
