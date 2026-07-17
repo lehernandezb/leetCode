05ad44d61e80c7c8748d954668571ebf8c9784d1
Luis <luis.hernandez1@ucalgary.ca>
2026-07-17 00:12:47 -0600

    Solution to question 31

--- 31.py
+++ 31.py
@@ -5,7 +5,23 @@ class Solution(object):
         :rtype: None Do not return anything, modify nums in-place instead.
         """
         
-        headptr = 0
-        tailptr = nums[:-1]
+        i = len(nums) - 2
 
+        while i >= 0 and nums[i] >= nums[i + 1]:
+            i -= 1
         
+        if i >= 0:
+            j = len(nums) - 1
+
+            while j >= 0 and nums[j] <= nums[i]:
+                j -= 1
+            
+            nums[i], nums[j] = nums[j], nums[i]
+        
+        def reverse(nums, start, end):
+            while start < end:
+                nums[start], nums[end] = nums[end], nums[start]
+                start += 1
+                end -= 1
+
+        reverse(nums, i + 1, len(nums) - 1)
