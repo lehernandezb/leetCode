28b9e5d4c0f62264aa44c8cd4615e07ae4114419
Luis <luis.hernandez1@ucalgary.ca>
2026-07-16 23:56:44 -0600

    working on 31

--- 31.py
+++ 31.py
@@ -5,3 +5,7 @@ class Solution(object):
         :rtype: None Do not return anything, modify nums in-place instead.
         """
         
+        headptr = 0
+        tailptr = nums[:-1]
+
+        
