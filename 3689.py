4dea50d75e77cf293910d6efdd1fba60a5781d10
Luis <luis.hernandez1@ucalgary.ca>
2026-07-21 23:18:21 -0600

    solution to 3689

--- 3689.py
+++ 3689.py
@@ -5,4 +5,7 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        
+     
+        max_value = max(nums)
+        min_value = min(nums)
+        return (max_value - min_value) * k
